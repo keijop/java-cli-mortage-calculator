929c450562f8cee08b41b658680f8ab26dd83bbd
Keijo Prunt <keijo.prunt@vincit.fi>
2022-08-14 12:23:28 +0300

    Add error handling/input validation

--- src/com/mortageCalculator/Main.java
+++ src/com/mortageCalculator/Main.java
@@ -11,14 +11,29 @@ public class Main {
         final byte PERCENTAGE = 100;
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Principal: ");
+        System.out.print("Principal (€1K - €1M): ");
         String principal = scanner.next();
+        while ((Float.parseFloat(principal) <  1000) || (Float.parseFloat(principal) > 10000000) ) {
+            System.out.println("Enter a value between 1000 - 1000000");
+            System.out.print("Principal (€1K - €1M): ");
+            principal = scanner.next();
+        }
 
         System.out.print("Annual interest rate: ");
         String yearlyRate = scanner.next();
+        while((Float.parseFloat(yearlyRate) <  1) || (Float.parseFloat(yearlyRate) > 30)){
+            System.out.println("Enter a value between 1 - 30: ");
+            System.out.print("Annual interest rate: ");
+            yearlyRate = scanner.next();
+        }
 
         System.out.print("Period (years): ");
         String period = scanner.next();
+        while((Float.parseFloat(period) <  1) || (Float.parseFloat(period) > 30)){
+            System.out.println("Enter a value between 1 - 30: ");
+            System.out.print("Period (years): ");
+            period = scanner.next();
+        }
 
         Float months = Float.parseFloat(period) / MONTHS_IN_YEAR;
         Float monthlyRate = Float.parseFloat(yearlyRate) / PERCENTAGE / MONTHS_IN_YEAR;

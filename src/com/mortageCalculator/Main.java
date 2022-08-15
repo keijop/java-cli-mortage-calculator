7cae7e449ce6cb76648027eba364d983afd8051e
Keijo Prunt <keijo.prunt@vincit.fi>
2022-08-15 06:16:45 +0300

    Refactor while loops and change variable types int to float

--- src/com/mortageCalculator/Main.java
+++ src/com/mortageCalculator/Main.java
@@ -11,34 +11,44 @@ public class Main {
         final byte PERCENTAGE = 100;
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Principal (€1K - €1M): ");
-        String principal = scanner.next();
-        while ((Float.parseFloat(principal) <  1000) || (Float.parseFloat(principal) > 10000000) ) {
-            System.out.println("Enter a value between 1000 - 1000000");
+        // to be able to use . symbol for entering decimal numbers, for ex. 3.29 would otherwise throw InputMismatchException in FI, ET etc. locales.
+        scanner.useLocale(Locale.ENGLISH);
+
+        int principal;1
+        float yearlyRate;
+        float period;
+
+        while (true) {
             System.out.print("Principal (€1K - €1M): ");
-            principal = scanner.next();
+            principal = scanner.nextInt();
+            if(principal >=  1000 && principal <= 1000000){
+                break;
+            }
+            System.out.println("Enter a value between 1000 - 1000000");
         }
 
-        System.out.print("Annual interest rate: ");
-        String yearlyRate = scanner.next();
-        while((Float.parseFloat(yearlyRate) <  1) || (Float.parseFloat(yearlyRate) > 30)){
-            System.out.println("Enter a value between 1 - 30: ");
+        while(true){
             System.out.print("Annual interest rate: ");
-            yearlyRate = scanner.next();
-        }
-
-        System.out.print("Period (years): ");
-        String period = scanner.next();
-        while((Float.parseFloat(period) <  1) || (Float.parseFloat(period) > 30)){
+            yearlyRate = scanner.nextFloat();
+            if(yearlyRate >=  1 && yearlyRate <= 30){
+                break;
+            }
             System.out.println("Enter a value between 1 - 30: ");
-            System.out.print("Period (years): ");
-            period = scanner.next();
         }
 
-        Float months = Float.parseFloat(period) / MONTHS_IN_YEAR;
-        Float monthlyRate = Float.parseFloat(yearlyRate) / PERCENTAGE / MONTHS_IN_YEAR;
-        Float payments = Float.parseFloat(period) * MONTHS_IN_YEAR;
-        Double monthlyMortgagePayment = Float.parseFloat(principal) * (monthlyRate * Math.pow((1 +  monthlyRate), payments)) / ( Math.pow((1 +  monthlyRate), payments)  - 1);
+        while(true){
+        System.out.print("Period (years): ");
+            period = scanner.nextInt();
+            if(period >=  1 && period <= 30){
+                break;
+            }
+            System.out.println("Enter a value between 1 - 30: ");
+        }
+
+        Float months = period / MONTHS_IN_YEAR;
+        Float monthlyRate = yearlyRate / PERCENTAGE / MONTHS_IN_YEAR;
+        Float payments = period * MONTHS_IN_YEAR;
+        Double monthlyMortgagePayment = principal * (monthlyRate * Math.pow((1 +  monthlyRate), payments)) / ( Math.pow((1 +  monthlyRate), payments)  - 1);
 
         NumberFormat formatter = NumberFormat.getCurrencyInstance(Locale.getDefault());
         System.out.print("Mortgage: " + formatter.format(monthlyMortgagePayment));
